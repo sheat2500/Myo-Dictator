d20b7baec6d216b8b7d46cd933a4f6a7682d23f8
Te <sheat2500@gmail.com>
2015-10-28 18:04:32 -0400

    push to new branch test

---
+++ app/src/main/java/com/lesmtech/dictator/SettingActivity.java
@@ -0,0 +1,18 @@
+package com.lesmtech.dictator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Rindt
+ * @version 0.1
+ * @since 10/28/15
+ */
+public class SettingActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
